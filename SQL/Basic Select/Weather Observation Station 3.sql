176b564242efaae741ecda1cf8c8a1b4f49b96f0
nvware <h_valizadgan@yahoo.com>
2023-03-03 23:49:42 +0200

    Create Weather Observation Station 3.sql

---
+++ SQL/Basic Select/Weather Observation Station 3.sql
@@ -0,0 +1,8 @@
+/*
+Enter your query here.
+Select distinct CITY From STATION where(id % 2) =0
+select distinct CITY from STATION where mod(ID,2)=0;
+*/
+
+
+Select distinct CITY From STATION where(id % 2) =0
